55c212ba640941118fe8bbe3e28451e6dbd0b091
ranjanjjp <ranjankumarsahujjp@gmail.com>
2026-01-21 15:18:07 +0530

    header

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+// ...existing code...
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    publicPath: "auto",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    open: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "headerApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/App.js",
+      },
+      shared: {
+        react: { singleton: true, eager: true, requiredVersion: false },
+        "react-dom": { singleton: true, eager: true, requiredVersion: false },
+      },
+    }),
+  ],
+};
+// ...existing code...
